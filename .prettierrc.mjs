f28a7fd17e6654546f9a079864cf7c0cdf921b2e
Mikayil Gacek <mikayil_gacek@icloud.com>
2025-06-18 17:46:48 +0200

    setup project and initial commit

---
+++ .prettierrc.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+export default {
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: 'avoid'
+};
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	build: {
+		outDir: 'dist',
+		emptyOutDir: true
+	}
+});
